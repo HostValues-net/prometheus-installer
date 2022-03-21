aedf16bab099e2581144d83701c525e97c3fcda1
Stijn Bannink <stijnbannink40@gmail.com>
2022-03-21 18:58:32 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+VERSION=v2.34.0
+VERSION_PATH=prometheus-2.34.0.linux-amd64
+BUILD=/opt
+
+# Setup the users and folders
+sudo useradd --no-create-home --shell /bin/false prometheus
+sudo useradd --no-create-home --shell /bin/false node_exporter
+sudo mkdir /etc/prometheus
+sudo mkdir /var/lib/prometheus
+
+# Update the user groups
+sudo chown prometheus:prometheus /etc/prometheus
+sudo chown prometheus:prometheus /var/lib/prometheus
+
+# Download Prometheus
+rm -rf $BUILD/$VERSION_PATH
+cd $BUILD
+wget https://github.com/prometheus/prometheus/releases/download/$VERSION/$VERSION_PATH.tar.gz
+tar -xvf $VERSION_PATH.tar.gz
+cd $VERSION_PATH
+
+# Copy binaries and update ownership
+sudo cp $BUILD/$VERSION_PATH/prometheus /usr/bin/
+sudo cp $BUILD/$VERSION_PATH/promtool /usr/bin/
+sudo chown prometheus:prometheus /usr/bin/prometheus
+sudo chown prometheus:prometheus /usr/bin/promtool
+
+# Copy console libraries and update ownership
+sudo cp -r $BUILD/$VERSION_PATH/consoles /etc/prometheus
+sudo cp -r $BUILD/$VERSION_PATH/console_libraries /etc/prometheus
+sudo cp -r $BUILD/$VERSION_PATH/prometheus.yml /etc/prometheus
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+sudo chown -R prometheus:prometheus /etc/prometheus/prometheus.yml
+
+echo -e "Installation done! Do not forget to make a systemd script! This is NOT support by this installation script yet."
